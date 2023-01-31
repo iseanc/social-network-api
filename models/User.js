04b7bf4f0c857d88b9150c5af27091a4cfb13fa7
Sean Collins <109634748+iseanc@users.noreply.github.com>
2023-01-31 16:16:40 -0700

    added friendCount and verified output

--- models/User.js
+++ models/User.js
@@ -3,29 +3,41 @@ const mongoose = require('mongoose');
 
 // Create instance of the Mongoose schema to define shape of each document
 // User:
-const userSchema = new mongoose.Schema({
-  // username, String, Unique, Required, Trimmed
-  username: { type: String, unique: true, required: true, trim: true },
-  // email, String, Required, Unique, Must match a valid email address (look into Mongoose's matching validation)
-  email: { type: String, unique: true, required: true, lowercase: true, match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/ },
-  // TODO: thoughts: Array of _id values referencing the Thought model
-  thoughts: [
-    { 
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: 'Thought'
+const userSchema = new mongoose.Schema(
+  {
+    // username, String, Unique, Required, Trimmed
+    username: { type: String, unique: true, required: true, trim: true },
+    // email, String, Required, Unique, Must match a valid email address (look into Mongoose's matching validation)
+    email: { type: String, unique: true, required: true, lowercase: true, match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/ },
+    // TODO: thoughts: Array of _id values referencing the Thought model
+    thoughts: [
+      { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Thought'
+      },
+    ],
+    // TODO: friends: Array of _id values referencing the User model (self-reference)
+    friends: [
+      { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User'
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
     },
-  ],
-  // TODO: friends: Array of _id values referencing the User model (self-reference)
-  friends: [
-    { 
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: 'User'
-    },
-  ],
-});
+    id: false,
+  }
+);
 
 // TODO: Schema Settings:
   // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
 
 // 'User' is the name of the model
 // userSchema is the name of the schema we are using to create a new instance of the model
